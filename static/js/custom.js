3ce4aefb65755a86caad1074f0e6c83379702912
x318846679 <1837865627@qq.com>
2025-11-14 15:44:03 +0800

    优化

--- static/js/custom.js
+++ static/js/custom.js
@@ -6,40 +6,54 @@ document.addEventListener('DOMContentLoaded', function () {
             fetch('/get_user/' + userId)
                 .then(response => response.json())
                 .then(data => {
-                    document.querySelector('#editUserForm [name="username"]').value = data.username;
-                    document.querySelector('#editUserForm [name="role"]').value = data.role;
-                    document.querySelector('#editUserForm').action = '/edit-user/' + userId;
+                    var usernameInput = document.querySelector('#editUserForm [name="username"]');
+                    var roleInput = document.querySelector('#editUserForm [name="role"]');
+                    var form = document.querySelector('#editUserForm');
+                    
+                    if (usernameInput && roleInput && form) {
+                        usernameInput.value = data.username;
+                        roleInput.value = data.role;
+                        form.action = '/edit-user/' + userId;
 
-                    var modal = new bootstrap.Modal(document.getElementById('editUserModal'));
-                    modal.show();
+                        var modal = new bootstrap.Modal(document.getElementById('editUserModal'));
+                        modal.show();
+                    }
                 })
                 .catch(err => console.error(err));
         });
     });
 
     // 处理编辑用户表单提交
-    document.querySelector('#editUserForm').addEventListener('submit', function (e) {
-        e.preventDefault();
-        var form = this;
-        var actionUrl = form.action;
-        var formData = new FormData(form);
+    var editUserForm = document.querySelector('#editUserForm');
+    if (editUserForm) {
+        editUserForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            var form = this;
+            var actionUrl = form.action;
+            var formData = new FormData(form);
 
-        fetch(actionUrl, {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.success)
-                if (data.success) {
-                    // 隐藏模态框
-                    var modal = bootstrap.Modal.getInstance(document.getElementById('editUserModal'));
-                    modal.hide(); // 隐藏模态框
-                    location.reload(); // 刷新页面以查看更改
-                } else {
-                    alert('更新失败');
-                }
+            fetch(actionUrl, {
+                method: 'POST',
+                body: formData
             })
-            .catch(err => console.error(err));
-    });
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.success)
+                    if (data.success) {
+                        // 隐藏模态框
+                        var modalElement = document.getElementById('editUserModal');
+                        if (modalElement) {
+                            var modal = bootstrap.Modal.getInstance(modalElement);
+                            if (modal) {
+                                modal.hide(); // 隐藏模态框
+                            }
+                        }
+                        location.reload(); // 刷新页面以查看更改
+                    } else {
+                        alert('更新失败');
+                    }
+                })
+                .catch(err => console.error(err));
+        });
+    }
 });
